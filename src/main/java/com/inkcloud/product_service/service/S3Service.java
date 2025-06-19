49ceef2ded25b1079bc2058c5a7f5a59e26bb780
PoorHotsix <majch98@gmail.com>
2025-06-19 18:20:31 +0900

    Change S3 service to signleton

--- src/main/java/com/inkcloud/product_service/service/S3Service.java
+++ src/main/java/com/inkcloud/product_service/service/S3Service.java
@@ -22,8 +22,7 @@ import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignReques
 @RequiredArgsConstructor
 public class S3Service {
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
+    private final S3Presigner s3Presigner;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
@@ -31,19 +30,9 @@ public class S3Service {
     public String generatePresignedUrl(String filename) {
         log.info("[S3Service] Presigned URL 생성 시작 - 파일명: {}", filename);
 
-        DefaultCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
-        AwsCredentials credentials = credentialsProvider.resolveCredentials();
-        log.info("[S3Service] 사용 중인 IAM 역할의 AccessKeyId: {}", credentials.accessKeyId());
-
-        S3Presigner presigner = S3Presigner.builder()
-            .region(Region.of(region))
-            .credentialsProvider(credentialsProvider)
-            .build();
-
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
             .bucket(bucketName)
             .key("products/" + filename)
-            //.acl("bucket-owner-full-control")
             .build();
 
         PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
@@ -51,10 +40,9 @@ public class S3Service {
             .signatureDuration(Duration.ofMinutes(5))
             .build();
 
-        URL url = presigner.presignPutObject(presignRequest).url();
+        URL url = s3Presigner.presignPutObject(presignRequest).url();
         log.info("[S3Service] Presigned URL 생성 완료: {}", url);
 
-        presigner.close();
         return url.toString();
     }
 }
